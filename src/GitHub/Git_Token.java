856e3ad65db9e5fa7a00616d3196e2e52d238a08
SamInst <sam105666@ceuma.com.br>
2022-06-23 11:47:53 -0300

    lembrar os comandos do git

--- src/GitHub/Git_Token.java
+++ src/GitHub/Git_Token.java
@@ -1,8 +1,4 @@
 package GitHub;
-
 public class Git_Token {
-    public static void main(String[] args) {
-        System.out.println("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");
-        System.out.println("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");
-    }
-}
+    public static void main(String[] args) {System.out.println("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");
+        System.out.println("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");}}
